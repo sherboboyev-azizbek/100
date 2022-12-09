882fd08e46fb9a42e0ff8eadd619d1216c9d0de3
sherboboyev-azizbek <azizkhannajot7@gmail.com>
2022-12-09 21:23:13 +0500

    Aziz

---
+++ js/main.js
@@ -0,0 +1,123 @@
+//! 1-dars
+// var userName = prompt("Ismingizni kiriting");
+// var userAge = prompt("Yoshingizni kiriting");
+// var userTemplate = `Assalomu alekum ${userName} sizning yilingiz ${2022- userAge}da`;
+// console.log(userTemplate);
+
+//! 2-dars
+
+//?   juft son
+// var x = prompt("son kiriting")
+
+// if (x % 2 == 0){
+//    console.log("bu juft son")
+// }
+
+//?   toq son
+
+// var x = prompt("son kiriting")
+
+// if (x % 2 !== 0){
+//    console.log("bu toq son")
+// }
+
+//? musbat manfiy 
+//  var x = prompt("son kiriting")
+
+//  if (x > 0 ){
+//    console.log("musbat son")
+//  } else if (x < 0) {
+//    console.log("manfiy son")
+//  }
+
+// var addNum = prompt("Son kiriting")
+
+//? Son kiritsa yoshi aytadi
+/*
+if(addNum >= 0 && addNum <= 10  ){
+   console.log("Chaqaloqsiz")
+} else if (addNum > 10 && addNum <=20) {
+   console.log("Boydoqsiz")
+}else if (addNum > 20 && addNum <=40) {
+   console.log("O'rta yoshdasiz")
+}else if (addNum > 40 && addNum <=70) {
+   console.log("Katta yoshdasiz")
+}else if (addNum > 70 ) {
+   console.log("Umriz uzoq bo'lsin")
+}
+else if (addNum < 0 ) {
+   console.log("Musbat son kiriting")
+}else{
+   console.log("Son kiriting")
+}
+*/
+
+//? Masala
+// Promptdan son kiritamiz juft toqligini aytadi
+/*
+var a = +prompt("Birinchi son kiriting")
+var b = 2;
+console.log(a % 2 == 0)
+*/
+// console.log(Math.round(5.49))
+
+//? Masala
+// var count = 16;
+
+// if (count != 15){
+//    console.log("Welcome!")
+// }else{
+//    console.log("Goodbye!")
+// }
+//? Masala -----Promptdan ism kiritamiz domga hosil buladi
+// var userName = prompt("Ismingizni kiriting");
+// var elSpan = document.querySelector(".user-name");
+// elSpan.textContent = userName;
+
+//? Masala-----Promptdan son kiritamiz jut bolsa juft toq bulsa toq chiqishi kerak consolda
+// var addNum = +prompt("Son kiriting");
+
+// if(addNum % 2 == 0){
+
+//    console.log(`${addNum} juft son`);
+// }else{ 
+//    console.log(`${addNum} toq son`)
+// }
+//? Masala----- 2 ta prompt buladi gender bilan name gender ayol bulsa xonm erkak bulsa aka boshladik gender male va female
+/* <h1>Assalomu alekum <span class="user-name"></span> <span class="status"></span> </h1> */
+// var userName = prompt("Ismingizni kiriting");
+// var gender = prompt("jinsingizni kiriting");
+
+// var elSpanName = document.querySelector(".user-name")
+// var elSpanStatus = document.querySelector(".status")
+
+// elSpanName.textContent = userName;
+// elSpanStatus.textContent = gender;
+
+
+// var e = "male"
+// var a = "female"
+
+// if(gender == e){
+// elSpanName.textContent = userName;
+// elSpanStatus.textContent = "aka";
+   
+// }else{
+// elSpanName.textContent = userName;
+// elSpanStatus.textContent = "opa";   
+// }
+
+//? Masala----- promptdan son kiritamiz puli safarga chiqadi
+// var numAdd = +prompt("Iltimos qancha puliz borligini dollorda kiriting");
+
+
+// var usd = 11000 * numAdd;
+// var euro = 12000;
+// var total = 9690000;
+
+// if (usd >= total ){
+//    console.log("Oq yo'l Alisher");
+// }else{
+//    console.log(" Alisher ozgina sabr qilishi kerak bular ekan!!!")
+// }
+//! 3-dars
